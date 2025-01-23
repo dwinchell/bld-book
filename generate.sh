1b6072669cfa9bffe77ccef3a863d12b8fe0c4a9
Dillon Winchell <dillonwinchell@gmail.com>
2025-01-23 01:04:21 -0500

    combat-dynamics

--- generate.sh
+++ generate.sh
@@ -3,4 +3,5 @@ cd "$SCRIPT_DIR" || exit 1
 
 rm export.yaml
 for f in $(ls -1 ./content); do echo '```' >> export.yaml; cat ./content/${f} >> export.yaml; done;
+echo '' >> export.yaml
 echo '```' >> export.yaml
