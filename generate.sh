7a73f18045bf9bfa033a5d55dfd98d0518e29b24
Dillon Winchell <dillonwinchell@gmail.com>
2025-01-23 12:53:56 -0500

    generate.sh

--- generate.sh
+++ generate.sh
@@ -2,6 +2,8 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 cd "$SCRIPT_DIR" || exit 1
 
 rm export.yaml
-for f in $(ls -1 ./content/*.yaml); do echo '```' >> export.yaml; cat ${f} >> export.yaml; done;
+
+echo '```' >> export.yaml
+for f in $(ls -1 ./content/*.yaml); do echo '---' >> export.yaml; cat ${f} >> export.yaml; done;
 echo '' >> export.yaml
 echo '```' >> export.yaml
