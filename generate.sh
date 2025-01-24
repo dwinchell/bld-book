99302bb6dcfca032f8ce7f87ee9d396d9f0c9583
Dillon Winchell <dillonwinchell@gmail.com>
2025-01-24 01:56:27 -0500

    script

--- generate.sh
+++ generate.sh
@@ -4,6 +4,6 @@ cd "$SCRIPT_DIR" || exit 1
 rm export.yaml
 
 echo '```' >> export.yaml
-for f in $(ls -1 ./content/*.yaml); do echo; echo '---' >> export.yaml; cat ${f} >> export.yaml; done;
+for f in $(ls -1 ./content/*.yaml); do echo '---' >> export.yaml; cat ${f} >> export.yaml; done;
 echo '' >> export.yaml
 echo '```' >> export.yaml
