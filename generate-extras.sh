88e29f73c257f234ec944bde6bccc7ccad9c75dd
Dillon Winchell <dillonwinchell@gmail.com>
2025-01-24 17:02:04 -0500

    Script

--- generate-extras.sh
+++ generate-extras.sh
@@ -1,11 +1,11 @@
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 cd "$SCRIPT_DIR" || exit 1
 
-FILENAME="export-outlines.yaml"
+FILENAME="export-extras.yaml"
 
 rm ${FILENAME}
 
 echo '```' >> ${FILENAME}
-for f in $(ls -1 ./content/outlines/*.yaml); do echo '---' >> ${FILENAME}; cat ${f} >> ${FILENAME}; done;
+for f in $(ls -1 ./content/extras/*.yaml); do echo '---' >> ${FILENAME}; cat ${f} >> ${FILENAME}; done;
 echo '' >> ${FILENAME}
 echo '```' >> ${FILENAME}
